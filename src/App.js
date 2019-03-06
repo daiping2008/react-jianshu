c7ee2f3c93515e2c82c68953b451d1fcb7ef3e50
daiping2008 <1170174401@qq.com>
2019-03-06 22:25:06 +0800

    '首页组件拆分'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import {Provider} from 'react-redux'
+import {BrowserRouter, Route} from 'react-router-dom'
 import store from './store'
 import {GlobalStyle} from './styled'
 import {IconFont} from './statics/iconfont/iconfont'
+
+import Home from './pages/home'
+import Detail from './pages/detail'
 
 import Header from './components/header'
 
@@ -14,6 +18,12 @@ class App extends Component {
           <GlobalStyle/>
           <IconFont/>
           <Header/>
+          <BrowserRouter>
+            <div>
+              <Route path="/" exact component={Home} ></Route>
+              <Route path="/detail" exact component={Detail} ></Route>
+            </div>
+          </BrowserRouter>
         </div>
       </Provider>
     );
