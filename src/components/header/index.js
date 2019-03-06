a7eae7ab9827af616177968d863d1a4b12053aa8
daiping2008 <1170174401@qq.com>
2019-03-06 15:21:47 +0800

    '添加actionCreators.js和actionTypes拆分数据管理'

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {CSSTransition} from 'react-transition-group'
 
+import { actionCreators }  from './store'
+
 import {
   HeaderWrapper,
   Logo,
@@ -58,23 +60,17 @@ class Header extends Component {
 
 const mapStateToProps = state => {
   return {
-    focused:state.focused
+    focused:state.header.focused
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     handleInputBlur(){
-      const action = {
-        type:'search_blur',
-      }
-      dispatch(action)
+      dispatch(actionCreators.searchBlur())
     },
     handleInputFocus(){
-      const action = {
-        type:'search_focus',
-      }
-      dispatch(action)
+      dispatch(actionCreators.searchFocus())
     }
   }
 }
