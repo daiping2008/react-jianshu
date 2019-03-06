7088b529c2ff0fb06d949280571077c571e46692
daiping2008 <1170174401@qq.com>
2019-03-06 13:12:21 +0800

    '项目初始化reset.css'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import {GlobalStyle} from './styled'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <GlobalStyle/>
+        <h1>22</h1>
       </div>
     );
   }
